c5daf62d1bc1c7d7bdff73d74f112e30186e13f6
尹恒岳 <yinhengyue@fotoable.com>
2020-01-19 12:05:06 +0800

    分离数据库备份脚本，做成单库和全库备份

---
+++ yhopssdk/pubilc/const.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*-coding:utf-8-*-
+"""
+Author : Yehe
+date   : 2020/1/19 11:58
+role   : 常量管理
+"""
+
+from enum import IntEnum as Enum
+
+
+class ConstError(TypeError):
+    pass
+
+
+class IntEnum(Enum):
+    @staticmethod
+    def find_enum(cls, value):
+        for k, v in cls._value2member_map_.items():
+            if k == value:
+                return v
+        return None
+
+
+class ErrorCode(IntEnum):
+    """ 错误码枚举 """
+
+    not_found = 404
+    bad_request = 400
+    unauthorized = 401
+    forbidden = 403
+    not_allowed = 405
+    not_acceptable = 406
+    conflict = 409
+    gone = 410
+    precondition_failed = 412
+    request_entity_too_large = 413
+    unsupport_media_type = 415
+    internal_server_error = 500
+    service_unavailable = 503
+    service_not_implemented = 501
+    handler_uncatched_exception = 504
+    config_import_error = 1001
+    config_item_notfound_error = 1002
+
+
+class _const(object):
+    def __setattr__(self, name, value):
+        if name in self.__dict__:
+            raise ConstError("Can't rebind const (%s)" % name)
+        if not name.isupper():
+            raise ConstError("Const must be upper.")
+        self.__dict__[name] = value
+
+
+const = _const()
+
+# AWS 配置
+const.AWS_ACCESS_KEY_ID = "AWS_ACCESS_KEY_ID"
+const.AWS_SECRET_ACCESS_KEY = "AWS_SECRET_ACCESS_KEY"
+# const.
+
