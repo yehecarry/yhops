c55a92641b94ff295f1353ea9704ce843fecd982
尹恒岳 <yinhengyue@fotoable.com>
2020-01-19 11:18:15 +0800

    分离mongodb备份脚本，做成单库和全库备份

--- yhopssdk/backup/dbbackup.py
+++ yhopssdk/backup/dbbackup.py
@@ -1,18 +1,10 @@
 #!/usr/bin/env python
 # -*-coding:utf-8-*-
 """
-author : Yehe
-date   : 2020/1/18 16:39
-role   : 
-"""
-#!/usr/bin/env python
-# -*-coding:utf-8-*-
-"""
 Author : Yehe
 date   : 2020/1/17 10:14
 role   : 备份数据库
 """
-
 
 import sys
 import os
@@ -63,7 +55,7 @@ class DbBackup(object):
 
     def mongodb_backup(self, mongodb_data):
         """
-        mongodb 备份方法
+        mongodb 单库备份方法
         :param user: mongodb 用户名
         :param password: mongodb 密码
         :param ip: mongodb ip地址
@@ -80,112 +72,126 @@ class DbBackup(object):
         database = mongodb_data.get("database")
         rep = mongodb_data.get("rep")
 
-        # 全库备份备份
-        if not database:
-            # 本地备份目录
-            local_backup_path = self.local_path + "/alldatabase"
-            # 备份文件路径
-            local_backup_file_path = self.local_path + "/" + "alldatabase.tar.gz"
-            # 创建备份目录
-            if not os.path.exists(local_backup_path):
-                exec_shell("mkdir -p {path}".format(path=local_backup_path))
-            # 单点全库备份
-            if not rep:
-                # 没有用户名与密码
-                if not user:
-                    exec_shell("mongodump -h {ip}:{port} -o {local_path}".format
-                               (ip=ip, port=port, database=database, local_path=local_backup_path))
-                    exec_shell("tar zcvf {local_ok_path} {local_path}"
-                               .format(local_ok_path=local_backup_file_path, local_path=local_backup_path))
-                    backup_path = local_backup_file_path[1:]
-                    return local_backup_file_path, backup_path
-                # 有用户名和密码
-                elif user and password:
-                    exec_shell("mongodump -u {user} -p {password} -h {ip}:{port} -o {local_path}".format
-                               (user=user, password=password, ip=ip, port=port, database=database,
-                                local_path=local_backup_path))
-                    exec_shell("tar zcvf {local_ok_path} {local_path}"
-                               .format(local_ok_path=local_backup_file_path, local_path=local_backup_path))
-                    backup_path = local_backup_file_path[1:]
-                    return local_backup_file_path, backup_path
-            # 集群单库备份
-            else:
-                # 生成集群ip 格式
-                cluster_ip = []
-                for i in range(len(ip)):
-                    cluster_ip.append(ip[i] + ":" + port[i])
-                cluster_ip = ",".join(cluster_ip)
-                # 没有用户名与密码
-                if not user:
-                    exec_shell("mongodump -h {cluster_ip}  -o {local_path}".format
-                               (cluster_ip=cluster_ip, database=database, local_path=local_backup_path))
-                    exec_shell("tar zcvf {local_ok_path} {local_path}"
-                               .format(local_ok_path=local_backup_file_path, local_path=local_backup_path))
-                    backup_path = local_backup_file_path[1:]
-                    return local_backup_file_path, backup_path
-                elif user and password:
-                    exec_shell("mongodump -u {user} -p {password} -h {cluster_ip} -o {local_path}"
-                               .format(user=user, password=password, cluster_ip=cluster_ip, database=database,
-                                       local_path=local_backup_path))
-                    exec_shell("tar zcvf {local_ok_path} {local_path}"
-                               .format(local_ok_path=local_backup_file_path, local_path=local_backup_path))
-                    backup_path = local_backup_file_path[1:]
-                    return local_backup_file_path, backup_path
-        # 单库备份
+        # 本地备份目录
+        local_backup_path = self.local_path + database
+        # 备份文件路径
+        local_backup_file_path = self.local_path + "/" + database + ".gz"
+        # 创建备份目录
+        if not os.path.exists(local_backup_path):
+            exec_shell("mkdir -p {path}".format(path=self.local_path))
+        # 单点单库备份
+        if not rep:
+            # 没有用户名与密码
+            if not user:
+                exec_shell("mongodump -h {ip}:{port} -d {database} -o {local_path}".format
+                           (ip=ip, port=port, database=database, local_path=local_backup_path))
+                exec_shell("tar zcvf {local_ok_path} {local_path}"
+                           .format(local_ok_path=local_backup_file_path, local_path=local_backup_path + database))
+                backup_path = local_backup_file_path[1:]
+                return local_backup_file_path, backup_path
+            # 有用户名和密码
+            elif user and password:
+                exec_shell("mongodump -u {user} -p {password} -h {ip}:{port} -d {database} -o {local_path}".format
+                           (user=user, password=password, ip=ip, port=port, database=database, local_path=local_backup_path))
+                exec_shell("tar zcvf {local_ok_path} {local_path}"
+                           .format(local_ok_path=local_backup_file_path, local_path=local_backup_path + database))
+                backup_path = local_backup_file_path[1:]
+                return local_backup_file_path, backup_path
+        # 集群单库备份
         else:
-            # 本地备份目录
-            local_backup_path = self.local_path + database
-            # 备份文件路径
-            local_backup_file_path = self.local_path + "/" + database + ".gz"
-            # 创建备份目录
-            if not os.path.exists(local_backup_path):
-                exec_shell("mkdir -p {path}".format(path=self.local_path))
-            # 单点单库备份
-            if not rep:
-                # 没有用户名与密码
-                if not user:
-                    exec_shell("mongodump -h {ip}:{port} -d {database} -o {local_path}".format
-                               (ip=ip, port=port, database=database, local_path=local_backup_path))
-                    exec_shell("tar zcvf {local_ok_path} {local_path}"
-                               .format(local_ok_path=local_backup_file_path, local_path=local_backup_path + database))
-                    backup_path = local_backup_file_path[1:]
-                    return local_backup_file_path, backup_path
-                # 有用户名和密码
-                elif user and password:
-                    exec_shell("mongodump -u {user} -p {password} -h {ip}:{port} -d {database} -o {local_path}".format
-                               (user=user, password=password, ip=ip, port=port, database=database, local_path=local_backup_path))
-                    exec_shell("tar zcvf {local_ok_path} {local_path}"
-                               .format(local_ok_path=local_backup_file_path, local_path=local_backup_path + database))
-                    backup_path = local_backup_file_path[1:]
-                    return local_backup_file_path, backup_path
-            # 集群单库备份
-            else:
-                # 生成集群ip 格式
-                cluster_ip = []
-                for i in range(len(ip)):
-                    cluster_ip.append(ip[i] + ":" + port[i])
-                cluster_ip = ",".join(cluster_ip)
-                # 没有用户名与密码
-                if not user:
-                    exec_shell("mongodump -h {cluster_ip} -d {database} -o {local_path}".format
-                               (cluster_ip=cluster_ip, database=database, local_path=local_backup_path))
-                    exec_shell("tar zcvf {local_ok_path} {local_path}"
-                               .format(local_ok_path=local_backup_file_path, local_path=local_backup_path + database))
-                    backup_path = local_backup_file_path[1:]
-                    return local_backup_file_path, backup_path
-                elif user and password:
-                    exec_shell("mongodump -u {user} -p {password} -h {cluster_ip} -d {database} -o {local_path}"
-                               .format(user=user, password=password, cluster_ip=cluster_ip, database=database,
-                                       local_path=local_backup_path))
-                    exec_shell("tar zcvf {local_ok_path} {local_path}"
-                               .format(local_ok_path=local_backup_file_path, local_path=local_backup_path + database))
-                    backup_path = local_backup_file_path[1:]
-                    return local_backup_file_path, backup_path
+            # 生成集群ip 格式
+            cluster_ip = []
+            for i in range(len(ip)):
+                cluster_ip.append(ip[i] + ":" + port[i])
+            cluster_ip = ",".join(cluster_ip)
+            # 没有用户名与密码
+            if not user:
+                exec_shell("mongodump -h {cluster_ip} -d {database} -o {local_path}".format
+                           (cluster_ip=cluster_ip, database=database, local_path=local_backup_path))
+                exec_shell("tar zcvf {local_ok_path} {local_path}"
+                           .format(local_ok_path=local_backup_file_path, local_path=local_backup_path + database))
+                backup_path = local_backup_file_path[1:]
+                return local_backup_file_path, backup_path
+            elif user and password:
+                exec_shell("mongodump -u {user} -p {password} -h {cluster_ip} -d {database} -o {local_path}"
+                           .format(user=user, password=password, cluster_ip=cluster_ip, database=database,
+                                   local_path=local_backup_path))
+                exec_shell("tar zcvf {local_ok_path} {local_path}"
+                           .format(local_ok_path=local_backup_file_path, local_path=local_backup_path + database))
+                backup_path = local_backup_file_path[1:]
+                return local_backup_file_path, backup_path
+
+    def mongodb_all_backup(self, mongodb_data):
+        """
+        mongodb 全库备份方法
+        :param user: mongodb 用户名
+        :param password: mongodb 密码
+        :param ip: mongodb ip地址
+        :param port: mongodb 端口
+        :param database: 备份的mongodb数据库名称
+        :param replSetName mongodb副本集名称默认是空
+        :return:
+        :param mongodb_data:
+        :return:
+        """
+        # 读取配置信息
+        user = mongodb_data.get("user")
+        password = mongodb_data.get("password")
+        ip = mongodb_data.get("ip")
+        port = mongodb_data.get("port")
+        rep = mongodb_data.get("rep")
+
+        # 本地备份目录
+        local_backup_path = self.local_path + "/alldatabase"
+        # 备份文件路径
+        local_backup_file_path = self.local_path + "/" + "alldatabase.tar.gz"
+        # 创建备份目录
+        if not os.path.exists(local_backup_path):
+            exec_shell("mkdir -p {path}".format(path=local_backup_path))
+        # 单点全库备份
+        if not rep:
+            # 没有用户名与密码
+            if not user:
+                exec_shell("mongodump -h {ip}:{port} -o {local_path}".format
+                           (ip=ip, port=port, local_path=local_backup_path))
+                exec_shell("tar zcvf {local_ok_path} {local_path}"
+                           .format(local_ok_path=local_backup_file_path, local_path=local_backup_path))
+                backup_path = local_backup_file_path[1:]
+                return local_backup_file_path, backup_path
+            # 有用户名和密码
+            elif user and password:
+                exec_shell("mongodump -u {user} -p {password} -h {ip}:{port} -o {local_path}".format
+                           (user=user, password=password, ip=ip, port=port, local_path=local_backup_path))
+                exec_shell("tar zcvf {local_ok_path} {local_path}"
+                           .format(local_ok_path=local_backup_file_path, local_path=local_backup_path))
+                backup_path = local_backup_file_path[1:]
+                return local_backup_file_path, backup_path
+        # 集群单库备份
+        else:
+            # 生成集群ip 格式
+            cluster_ip = []
+            for i in range(len(ip)):
+                cluster_ip.append(ip[i] + ":" + port[i])
+            cluster_ip = ",".join(cluster_ip)
+            # 没有用户名与密码
+            if not user:
+                exec_shell("mongodump -h {cluster_ip}  -o {local_path}".format(cluster_ip=cluster_ip,
+                                                                               local_path=local_backup_path))
+                exec_shell("tar zcvf {local_ok_path} {local_path}"
+                           .format(local_ok_path=local_backup_file_path, local_path=local_backup_path))
+                backup_path = local_backup_file_path[1:]
+                return local_backup_file_path, backup_path
+            elif user and password:
+                exec_shell("mongodump -u {user} -p {password} -h {cluster_ip} -o {local_path}"
+                           .format(user=user, password=password, cluster_ip=cluster_ip, local_path=local_backup_path))
+                exec_shell("tar zcvf {local_ok_path} {local_path}"
+                           .format(local_ok_path=local_backup_file_path, local_path=local_backup_path))
+                backup_path = local_backup_file_path[1:]
+                return local_backup_file_path, backup_path
 
     def redis_backup(self):
-        pass
+        print("redisrest")
 
 
 if __name__ == '__main__':
-    # main()
-    pass
+    print("test")
